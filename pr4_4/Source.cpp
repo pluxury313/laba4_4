123358b628f7d5e6a8a2a74206159a815e04e694
pluxury313 <vadim31235@gmail.com>
2022-11-04 11:11:57 +0200

    first commit

---
+++ pr4_4/Source.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, R{}, B, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		if (x <= 0)
+			B = (-R * x + 6 * R) / 6;
+		else
+			if (0 < x && x <= R)
+				B = -sqrt(R * R - x * x);
+			else
+				if (R < x && x <= 2 * R)
+					B = sqrt(x * x - 4 * R * x + 3 * R * R);
+				else
+					B = R;
+		y = B;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
